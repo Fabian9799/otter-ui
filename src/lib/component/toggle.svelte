3ca9850bf3ef7ac7d4fbf360f1a794aaf5bd9158
fabian9799 <fabian9799@protonmail.ch>
2022-11-11 17:44:27 +0100

    ✨ fixed checkbox for firefox, rewritten toggle

--- src/lib/component/toggle.svelte
+++ src/lib/component/toggle.svelte
@@ -1,14 +1,15 @@
-<div class="relative h-10 w-[5rem]">
-	<label for="checkbox" class="relative">
+<div>
+	<div class="relative inline-block w-[5rem] h-10 align-middle select-none">
+		<label for="toggle" class=" sr-only">Toggle</label>
 		<input
-			name="checkbox"
-			class="peer absolute top-0 h-10 w-[5rem] appearance-none rounded-md border-2 border-gray-800 bg-transparent p-2 hover:border-gray-700 focus:border-gray-700 focus:outline-0"
 			type="checkbox"
+			name="toggle"
+			id="toggle"
+			class="peer absolute block w-5 h-5 m-2.5 rounded-full border-none bg-gray-700 checked:ml-12 border-4 appearance-none cursor-pointer duration-100"
 		/>
-		<div
-			class="absolute flex h-10 w-10 rounded-md bg-gray-800 p-1 duration-200 hover:bg-gray-700 peer-checked:translate-x-10 peer-checked:bg-gray-700"
-		>
-			<div class="my-auto mx-auto h-5 w-5 rounded-full bg-gray-600" />
-		</div>
-	</label>
+		<label
+			for="toggle"
+			class="block overflow-hidden h-10 rounded-md border-2 border-gray-800   peer-checked:bg-gray-800 cursor-pointer duration-100 hover:border-gray-700 focus:border-gray-700 focus:outline-0"
+		/>
+	</div>
 </div>
