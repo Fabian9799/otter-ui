ed757ec0ea79cf9e2588431474fca2babf200ff4
fabian9799 <fabian9799@protonmail.ch>
2022-11-12 15:14:36 +0100

    💄 changed toggle duration

--- src/lib/component/toggle.svelte
+++ src/lib/component/toggle.svelte
@@ -5,11 +5,11 @@
 			type="checkbox"
 			name="toggle"
 			id="toggle"
-			class="peer absolute block w-5 h-5 m-2.5 rounded-full border-none bg-gray-700 checked:ml-12 border-4 appearance-none cursor-pointer duration-100"
+			class="peer absolute block w-5 h-5 m-2.5 rounded-full border-none bg-gray-700 checked:ml-12 border-4 appearance-none cursor-pointer duration-200"
 		/>
 		<label
 			for="toggle"
-			class="block overflow-hidden h-10 rounded-md border-2 border-gray-800   peer-checked:bg-gray-800 cursor-pointer duration-100 hover:border-gray-700 focus:border-gray-700 focus:outline-0"
+			class="block overflow-hidden h-10 rounded-md border-2 border-gray-800   peer-checked:bg-gray-800 cursor-pointer duration-200 hover:border-gray-700 focus:border-gray-700 focus:outline-0"
 		/>
 	</div>
 </div>
